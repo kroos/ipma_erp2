cb0bf5a4be96eeea6e7836364cc1fad51584d9d2
kroos <dhiauddin@gmail.com>
2025-01-22 19:09:04 +0800

    -- fix layouts backup

--- resources/views/layouts--/notification.blade.php
+++
@@ -1,1 +0,0 @@
-<a class="dropdown-item" href="#"><i class="fa-regular fa-comment"></i> Notifications</a>
--- resources/views/layouts--/sessionerrors.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- IF ERROR -->
-@if(Session::has('danger'))
-	<h6 class="pb-4 mb-4 border-bottom text-center alert alert-danger">
-		{{ Session::get('danger') }}
-	</h6>
-@endif
