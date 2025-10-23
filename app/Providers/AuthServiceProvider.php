bfd84a2b1edc0c918197c8813c49916c7c1af16d
Dhiauddin <dhiauddin@gmail.com>
2025-10-23 23:09:55 +0800

    -- update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,7 +23,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         // using this to override Illuminate\Auth\EloquentUserProvider
         \Illuminate\Support\Facades\Auth::provider('loginuserprovider', function($app, array $config) {
-            return new Auth\EloquentUserProvider($app['hash'], $config['model']);
+            return new \App\Providers\Auth\EloquentUserProvider($app['hash'], $config['model']);
         });
     }
 }
